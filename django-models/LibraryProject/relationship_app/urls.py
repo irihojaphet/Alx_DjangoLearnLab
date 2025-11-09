2546360302dc13f023efb37f799b5103fa7224c7
irihojaphet <irihojaja@yahoo.com>
2025-11-09 17:36:51 -0500

    adding new views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from . import views
+from .views import list_books
 
 app_name = 'relationship_app'
 
@@ -21,7 +22,7 @@ urlpatterns = [
     path('librarian-view/', views.librarian_view, name='librarian_view'),
     path('member-view/', views.member_view, name='member_view'),
     
-    # Book CRUD views with permissions - UPDATED PATHS
+    # Book CRUD views with permissions
     path('add_book/', views.add_book, name='add_book'),
     path('edit_book/<int:pk>/', views.edit_book, name='edit_book'),
     path('delete_book/<int:pk>/', views.delete_book, name='delete_book'),

7ad9dad8705e94b727b3d40e68f3104293506e80
irihojaphet <irihojaja@yahoo.com>
2025-11-09 23:26:39 +0200

    adding new views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,18 +1,18 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import list_books, LibraryDetailView, register
+from . import views
 
 app_name = 'relationship_app'
 
 urlpatterns = [
     # Function-based view: List all books
-    path('books/', list_books, name='list_books'),
+    path('books/', views.list_books, name='list_books'),
     
     # Class-based view: Library detail
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
     
     # Authentication views
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('register/', register, name='register'),
+    path('register/', views.register, name='register'),
 ]

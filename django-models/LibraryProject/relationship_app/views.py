03e62bef6c5a63d2114714b7cd9e4f95b1a6329d
irihojaphet <irihojaja@yahoo.com>
2025-11-09 17:42:49 -0500

    adding new views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,8 @@ from django.views.generic.detail import DetailView
 from django.views.generic import ListView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
-from django.contrib.auth.decorators import user_passes_test, permission_required
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import permission_required
 from django.contrib import messages
 from .models import Book
 from .models import Library
